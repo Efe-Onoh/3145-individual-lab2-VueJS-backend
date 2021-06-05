feceb0e92883bd5ef8bb170ed7e61235f7ca6654
Efe O <ednaellenpope@gmail.com>
2021-06-06 02:39:40 +0400

    debugging put route

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/collection/:collectionName/:id', (req, res, next) => {
 app.put('/collection/:collectionName/:subject', (req, res, next) => {
     req.collection.update(
     {subject: req.params.subject},
-    {$set: req.body},
+    {$set: {numberOfSpaces: 1}},
     {safe: true, multi: false},
     (e, result) => {
     if (e) return next(e)
